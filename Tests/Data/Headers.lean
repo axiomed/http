f43bdab864d71a3af2982a172262dfc8c04ffc95
Anton Parkhomenko <mailbox@chuwy.me>
2023-12-15 20:56:47 +0700

    chore: describe Prod instance

--- Tests/Data/Headers.lean
+++ Tests/Data/Headers.lean
@@ -6,7 +6,9 @@ import Soda.Grape.Text
 import Http.Data.Headers
 import «Tests».Framework
 
--- A bit non-sense instance
+-- This instance might look like non-sense,
+-- but it should be enough for real-world tasks as
+-- `Result.cont` is never what you want to compare
 instance [BEq a] : BEq (Result (a × a)) where
   beq
     | Result.done i is, Result.done j js => i == j && is.toASCIIString == js.toASCIIString
@@ -15,7 +17,11 @@ instance [BEq a] : BEq (Result (a × a)) where
     | _, _ => false
 
 def headersTests : List Assertion := [
-  Assertion.make "Headers.isEmpty returns true on Request.empty" true Headers.empty.isEmpty,
+  Assertion.make "Headers.isEmpty returns true on Request.empty"
+    true
+    Headers.empty.isEmpty,
 
-  Assertion.make "Headers.isEmpty returns true on Request.empty" (Result.done ("foo", "bar") default) (Grape.run Headers.headerParser ("foo: bar".toSlice))
+  Assertion.make "Headers.headerParser can parse `foo: bar`"
+    (Result.done ("foo", "bar") default)
+    (Grape.run Headers.headerParser ("foo: bar".toSlice))
 ]
